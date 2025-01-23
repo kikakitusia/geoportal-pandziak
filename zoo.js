97cdcc7f3a730807af00939efc3a446974849543
kikakitusia <weronikaklimkowicz1@gmail.com>
2025-01-23 12:02:24 +0100

    Add files via upload

---
+++ zoo.js
@@ -0,0 +1,309 @@
+var zooData = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "ZOO Wrocław Sp. z o.o.",
+                "pandkamala": "TAK",
+                "link": "https://zoo.wroclaw.pl/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [17.074848424546158,51.10425535]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "ZOO FARMA",
+                "pandkamala": "NIE",
+                "link": "https://zoolaczna.pl/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [16.141054618189433,50.6674791]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Ogród Zoologiczny w Lubinie",
+                "pandkamala": "NIE",
+                "link": "https://zoolubin.pl/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [16.212656507778096,51.3930603]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Ogród Zoologiczny w Bydgoszczy",
+                "pandkamala": "NIE",
+                "link": "https://zoo.bydgoszcz.com/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [18.03295229453267,53.162501750000004]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Ogród Zoologiczny w Toruniu",
+                "pandkamala": "TAK",
+                "link": "https://www.zoo.torun.pl/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [18.59075241787857,53.0089283]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Ogród Zoologiczny im. Stefana Milera w Zamościu",
+                "pandkamala": "NIE",
+                "link": "https://www.zoo.zamosc.pl/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [23.239248035039434, 50.717477849999995]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Miejski Ogród Zoologiczny w Łodzi Sp. z o.o.",
+                "pandkamala": "TAK",
+                "link": "https://orientarium.lodz.pl/?srsltid=AfmBOoo0LIT_x3g94OJyMXBP1z31rAy5Ga4yHNxtTsG8yxg9LJM4omfA"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [19.412581480099202,	51.76102160000001]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "ZOO Safari Borysew",
+                "pandkamala": "NIE",
+                "link": "https://zoosafari.com.pl/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [18.9416499739335,51.881754]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Miejski Park i Ogród Zoologiczny w Krakowie",
+                "pandkamala": "TAK",
+                "link": "https://zoo-krakow.pl/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [19.84951100653447,50.05351315]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Miejski Ogród Zoologiczny w Płocku",
+                "pandkamala": "TAK",
+                "link": "http://www.zoo.plock.pl/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [19.701869826185703,	52.535503750000004]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Miejski Ogród Zoologiczny w Warszawie",
+                "pandkamala": "TAK",
+                "link": "https://zoo.waw.pl/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [21.021971782196573,52.2585469]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Ogród Zoologiczny Opole",
+                "pandkamala": "TAK",
+                "link": "https://zoo.opole.pl/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [17.92789503509831,50.6523448]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Akcent ZOO",
+                "pandkamala": "NIE",
+                "link": "https://www.schronisko.bialystok.pl/content/category/5-akcent-zoo"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [23.165792953941907,53.120484399999995]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Akwarium Gdyńskie",
+                "pandkamala": "NIE",
+                "link": "https://akwarium.gdynia.pl/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [18.55731987107607,54.51832735]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Miejski Ogród Zoologiczny Wybrzeża",
+                "pandkamala": "TAK",
+                "link": "https://zoo.gdansk.pl/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [18.530805081460038,54.4144386]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Ogród Zoologiczny CANPOL",
+                "pandkamala": "NIE",
+                "link": "https://zoo.canpol.pl/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [17.316114237095796,53.65053075]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Ogród Zoologiczny Dolina Charlotty",
+                "pandkamala": "NIE",
+                "link": "http://zoo.charlotta.pl/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [16.94920165642158,54.5298451]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Śląski Ogród Zoologiczny w Chorzowie",
+                "pandkamala": "TAK",
+                "link": "https://slaskiezoo.pl/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [18.994153082576325,50.282247600000005]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Leśny Park Niespodzianek",
+                "pandkamala": "NIE",
+                "link": "https://lesnypark.pl/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [18.832575327804566,49.7170651]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Ogród Zoologiczny w Sosnowcu",
+                "pandkamala": "NIE",
+                "link": "https://www.cee-egzotarium.sosnowiec.pl/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [19.243931750765164,50.300218799999996]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Ogród Zoologiczny w Lisowie",
+                "pandkamala": "NIE",
+                "link": "http://www.lesne-zacisze.net/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [20.641342998625124,50.6887973]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Park Dzikich Zwierząt w Kadzidłowie",
+                "pandkamala": "NIE",
+                "link": "https://kadzidlowo-park.pl/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [21.491988332955536,53.7134027]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Ogród Zoologiczny - Poznań",
+                "pandkamala": "TAK",
+                "link": "https://zoo.poznan.pl/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [17.005797229267692,52.39974495]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Ogród Zoologiczny - Poznań (Stare Zoo)",
+                "pandkamala": "NIE",
+                "link": "https://zoo.poznan.pl/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [16.90495531261694, 52.408194949999995]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "nazwa": "Ogród Zoologiczny w Nowym Tomyślu",
+                "pandkamala": "NIE",
+                "link": "https://zoonowytomysl.pl/"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [16.139586697957746,52.310363800000005]
+            }
+        }
+
+
+
+
+    ]
+};
